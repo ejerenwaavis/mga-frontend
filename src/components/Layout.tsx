925885b260c15e76dc84fd4a3465c2d1b7cb5da3
meadgreenautosatl-dev <meadgreenautosatl@gmail.com>
2026-04-13 23:53:34 +0100

    Increase logo image height to 300px
    Updated logo image size for better visibility.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -169,7 +169,7 @@ function Navbar() {
           <img
             src="/MGA-FULL-LOGO.svg"
             alt="Mead Green Autos Logo"
-className="h-[50px] w-auto md:h-[50px]"  // 90% of original size
+className="h-[300px] w-auto md:h-[300px]"  // 90% of original size
           // h-8 (32px) for mobile, md:h-10 (40px) for desktop
           />
           {/* <span
