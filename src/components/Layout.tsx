b2e919ded7ed1574926cfcbec3e9bbc5a7b64c45
meadgreenautosatl-dev <meadgreenautosatl@gmail.com>
2026-04-14 07:12:17 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -336,7 +336,7 @@ function Footer() {
           </div>
 
           {/* Column 4 */}
-          <div className="lg:pr-0"> 
+         <div className="lg:-mr-2"> 
             <h4 className="mb-4 text-xs font-semibold uppercase tracking-widest text-secondary-foreground/50">
               Contact
             </h4>
